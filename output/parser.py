638ebe8878cf4a9586a756d9e98d0631ea542c57
Pranathi Tupakula <ptupakula3@gatech.edu>
2015-11-07 22:54:38 -0500

    Added features to parser

--- output/parser.py
+++ output/parser.py
@@ -29,20 +29,39 @@ text_json = json.loads(text, cls=ConcatJSONDecoder)
 total_confidence = text_json[len(text_json) - 1]['results'][0]['alternatives'][0]['confidence']
 
 if total_confidence <= 0.64:
-	print "*****BAD INPUT: TOO MUCH NOISE. PLEASE TRY AGAIN.*****"
-	exit()
+    print "*****BAD INPUT: TOO MUCH NOISE. PLEASE TRY AGAIN.*****"
+    exit()
 
 #This is a list of ['word', start_time, end_time]
 timestamps_list = text_json[len(text_json) - 1]['results'][0]['alternatives'][0]['timestamps']
+#print(timestamps_list)
 
 #This is a list of ['word', confidence_value]
 confidence_list = text_json[len(text_json) - 1]['results'][0]['alternatives'][0]['word_confidence']
+#print (confidence_list)
 
 """
 TODO:
 1. Eliminate the words from timestamps_list that have a confidence value (which you can get from confidence_list) lower than 0.65 (our threshold)
 2. Find the average time difference between a word and the next word. (start_time(word n+1) - end_time(word n), for all n words in the new timestamps_list)
 """
+for word, confidence in confidence_list:
+    if confidence < 0.65:
+        confidence_list.remove([word, confidence])
 
+for word, start_time, end_time in timestamps_list:
+    b = False
+    for x in confidence_list:
+        if x[0] == word:
+            b = True
+    if not b:
+        timestamps_list.remove([word, start_time, end_time])
 
+end_time_old = timestamps_list[0][1]
+total_time = 0
+for word, start_time, end_time in timestamps_list:
+    print(start_time - end_time_old)
+    total_time += start_time - end_time_old
+    end_time_old = end_time
 
+avg_time = total_time/len(timestamps_list)
